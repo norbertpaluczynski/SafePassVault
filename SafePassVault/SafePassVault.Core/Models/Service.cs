94ec03c3668f6e69d84b781059164778f98886aa
Michał Ratajczak <michalratajczak2105@gmail.com>
2020-03-15 16:43:59 +0100

    Created prototype version of models

---
+++ SafePassVault/SafePassVault.Core/Models/Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafePassVault.Core.Models
+{
+    public class Service
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+    }
+}
