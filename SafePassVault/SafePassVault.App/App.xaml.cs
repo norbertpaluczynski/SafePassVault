35475ef727aa5d245f3de39d13e6ad7d58b981b7
Michał Ratajczak <michalratajczak2105@gmail.com>
2020-03-15 12:46:26 +0100

    Created LoginWindow.

---
+++ SafePassVault/SafePassVault.App/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SafePassVault.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void TopBar_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ((Window)((DockPanel)sender).Tag).DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            ((Window)((Button)sender).Tag).Close();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var w = ((Window)((Button)sender).Tag).WindowState = WindowState.Minimized;
+        }
+    }
+}
