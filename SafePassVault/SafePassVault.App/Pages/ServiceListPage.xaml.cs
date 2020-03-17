da1153bb5a576a06284772455e99832940096d78
Michał Ratajczak <michalratajczak2105@gmail.com>
2020-03-17 19:39:49 +0100

    Created ServiceListPage

---
+++ SafePassVault/SafePassVault.App/Pages/ServiceListPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using SafePassVault.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SafePassVault.App.Pages
+{
+    /// <summary>
+    /// Interaction logic for ServiceListPage.xaml
+    /// </summary>
+    public partial class ServiceListPage : Page
+    {
+        public ServiceListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
