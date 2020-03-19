abfad0e2eb50108aaa573847190e8ae2e05b98e5
Michał Ratajczak <michalratajczak2105@gmail.com>
2020-03-19 21:42:43 +0100

    Created password generator.

---
+++ SafePassVault/SafePassVault.Core/Helpers/AppSettings.cs
@@ -0,0 +1,20 @@
+﻿using SafePassVault.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafePassVault.Core.Helpers
+{
+    public static class AppSettings
+    {
+        public static PasswordCharsetPreset DefaultPasswordCharsetPreset => new PasswordCharsetPreset
+        {
+            PresetName = "Default",
+            AllowNumbers = true,
+            AllowLowercaseLetters = true,
+            AllowUppercaseLetters = true,
+            AllowSpecialCharacters = true,
+            AllowSpace = true
+        };
+    }
+}
---
+++ SafePassVault/SafePassVault.Core/Helpers/PasswordCharsets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafePassVault.Core.Helpers
+{
+    public static class PasswordCharsets
+    {
+        public static string Numbers => "0123456789";
+        public static string LowercaseLetters => "abcdefghijklmnopqrstuvwxyz";
+        public static string UppercaseLetters => "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static string SpecialCharacters => "!@#$%^&*()-=_+`~[]{};:\\|,<.>/?\"*";
+        public static string Space => " ";
+    }
+}
